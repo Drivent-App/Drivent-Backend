7e4defbd203f9218e66264ad0e7ddc078682249a
MolinaMS95 <matheusmolina95@gmail.com>
2023-03-11 16:59:14 -0300

    feat: redis implementation on get event

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,17 @@
-import { prisma } from "@/config";
+import { prisma, redis } from "@/config";
+import { Event } from "@prisma/client";
 
 async function findFirst() {
-  return prisma.event.findFirst();
+  const redisEvent = await redis.get("event");
+  let event: Event = JSON.parse(redisEvent);
+  if(event) {
+    console.log("peguei do redis");
+  }
+  if (!event) {
+    event = await prisma.event.findFirst();
+    await redis.set("event", JSON.stringify(event));
+  }
+  return event;
 }
 
 const eventRepository = {
